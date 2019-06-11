d13b8892ffffa97cd2aefdceb0b0be1bfc3f2492
Sarvesh Jain <sarvesh@ost.com>
2019-06-11 17:22:08 +0530

    Setting CORS header for OPTIONS type request

--- src/Server.ts
+++ src/Server.ts
@@ -11,6 +11,7 @@ import Interaction from './Interaction';
 import Logger from './Logger';
 import ServerError from './ServerError';
 import EnoughFundException from './Faucet/EnoughFundException';
+
 import HttpStatus = require('http-status-codes');
 
 /** A map of chain Ids to their respective faucet. */
@@ -127,11 +128,19 @@ export default class Server {
   ): Promise<void> {
     let body: Uint8Array[];
 
-    /* Allow cross origin request, this is needed to build an UI on top of faucet */
-    response.setHeader('Access-Control-Allow-Origin', process.env.MOSAIC_FAUCET_CORS_ORIGIN || '*');
-    response.setHeader('Access-Control-Request-Method', process.env.MOSAIC_FAUCET_CORS_REQUEST_METHOD || '*');
-    response.setHeader('Access-Control-Allow-Methods', process.env.MOSAIC_FAUCET_CORS_ALLOW_METHOD || 'POST, GET, OPTIONS');
-    response.setHeader('Access-Control-Allow-Headers', process.env.MOSAIC_FAUCET_CORS_ALLOW_HEADERS || '*');
+    const corsHeaders = {
+      'Access-Control-Allow-Origin': process.env.MOSAIC_FAUCET_CORS_ORIGIN || '*',
+      'Access-Control-Request-Method': process.env.MOSAIC_FAUCET_CORS_REQUEST_METHOD || '*',
+      'Access-Control-Allow-Methods': process.env.MOSAIC_FAUCET_CORS_ALLOW_METHOD || 'POST, GET, OPTIONS',
+      'Access-Control-Allow-Headers': process.env.MOSAIC_FAUCET_CORS_ALLOW_HEADERS || '*',
+    };
+
+    /* Allow cross origin request, this is needed to build an UI on top of the faucet */
+    if (request.method === 'OPTIONS') {
+      response.writeHead(HttpStatus.NO_CONTENT, corsHeaders);
+      response.end();
+      return;
+    }
 
     try {
       body = await Server.readBody(request);
